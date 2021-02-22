84eda1ff2dc10bac016f0193fc8b7d950758e85a
Jennifer Kramer <jenkrame@gmail.com>
2021-02-22 17:14:59 -0500

    Added more to knexfile.js file. Created and filled out index.js file. Added semi rule to eslintrc file. Created connection.js file in database folder. Created migrations and seeds folders in database folder. Created api folder.

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,40 @@
 // Update with your config settings.
 
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './database/auth.db3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
     },
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: pgConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
   }
-
 };
